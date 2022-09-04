a92be9608c5449224fdb57b196170ce13c14f5fe
arc-015 <chavanavinash015@gmail.com>
2022-09-04 22:11:08 +0530

    "Create a board of Tic Toc Tie Game".

--- Day7tictoctie/src/Tic_toc_tieGame.java
+++ Day7tictoctie/src/Tic_toc_tieGame.java
@@ -3,7 +3,17 @@ public class Tic_toc_tieGame {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Toc tie Game ");
-		
-		
+
+		char[] board = createBoard();
+		System.out.println("Welcome to TicTocToe Simulation Game");
+
+	}
+
+	private static char[] createBoard() {
+		char[] board = new char[10];
+		for (int index = 0; index < board.length; index++) {
+			board[index] = ' ';
+		}
+		return board;
 	}
 }
